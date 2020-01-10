7ffcede982e46fcc7945f37ee5d175a7bac421af
ajisunta <ajisunta076@gmail.com>
2020-01-10 14:14:21 +0700

    anjg

--- finalprojekajis.cpp
+++ finalprojekajis.cpp
@@ -15,11 +15,13 @@ struct stc_hp{
 	char stock[8];		
 	char merek[20];
 	char price[30];	
-}listhp[6];					
+}listhp[100];					
 
 void CLEAR_SCREEN(){
 	if (system("CLS")) system("clear");
 };
+
+FILE	*f_menu;
 
 char PRINT_TABLE_HEAD(){
 	printf("================================================================================\n");
@@ -49,9 +51,9 @@ void READ_BY_DEFAULT(){
 	READ_DEFAULT();
 	
 	int i = 0;
-	int size = sizeof(listhp)/sizeof(listhp[0]);
+//	int size = sizeof(listhp)/sizeof(listhp[0]);
 	PRINT_TABLE_HEAD();	
-	for(i = 0; i < size; i++) {
+	for(i = 0; i < sizeof(f_menu); i++) {
 		PRINT_TABLE_BODY(i+1, listhp[i].hp, listhp[i].kode, listhp[i].stock, listhp[i].merek, listhp[i].price);
 	}
 	PRINT_TABLE_FOOTER();
@@ -62,11 +64,11 @@ void READ_BY_NAME(){
 	
 	int j, i;
 	char hp_temp[69], kode_temp[69], stock_temp[69], merek_temp[69], price_temp[69];
-	int size = sizeof(listhp)/sizeof(listhp[0]);
+//	int size = sizeof(listhp)/sizeof(listhp[0]);
 	PRINT_TABLE_HEAD();
 	
-	for(i=0; i<size; i++){
-		for(j=i+1; j<=size; j++){
+	for(i=0; i<sizeof(f_menu); i++){
+		for(j=i+1; j<=sizeof(f_menu); j++){
 			if(strcmp(listhp[i].hp, listhp[j].hp)>0){
 				strcpy(hp_temp, listhp[i].hp);
             	strcpy(listhp[i].hp, listhp[j].hp);
@@ -91,7 +93,7 @@ void READ_BY_NAME(){
 		}
 	}
 	
-	for(i=1; i<=size; i++){
+	for(i=1; i<=sizeof(f_menu); i++){
 		PRINT_TABLE_BODY(i, listhp[i].hp, listhp[i].kode, listhp[i].stock, listhp[i].merek, listhp[i].price);
 	}
 	
@@ -103,11 +105,11 @@ void READ_BY_PRICE(){
 	
 	int j, i, pinjam, pinjam2;
 	char hp_temp[69], kode_temp[69], stock_temp[69], merek_temp[69], price_temp[69];
-	int size = sizeof(listhp)/sizeof(listhp[0]);
+//	int size = sizeof(listhp)/sizeof(listhp[0]);
 	PRINT_TABLE_HEAD();
 	
-	for(i=0; i<size; i++){
-		for(j=i+1; j<=size; j++){
+	for(i=0; i<sizeof(f_menu); i++){
+		for(j=i+1; j<=sizeof(f_menu); j++){
 			if(strcmp(listhp[i].price, listhp[j].price)>0){
 				strcpy(hp_temp, listhp[i].hp);
             	strcpy(listhp[i].hp, listhp[j].hp);
@@ -132,7 +134,7 @@ void READ_BY_PRICE(){
 		}
 	}
 	
-	for(i=1; i<=size; i++){
+	for(i=1; i<=sizeof(f_menu); i++){
 		PRINT_TABLE_BODY(i, listhp[i].hp, listhp[i].kode, listhp[i].stock, listhp[i].merek, listhp[i].price);
 	}
 	
@@ -143,14 +145,14 @@ void SEARCH_listhp(){
 	READ_DEFAULT();
 	
 	char keyword[69];
-	int size = sizeof(listhp)/sizeof(listhp[0]);
+//	int size = sizeof(listhp)/sizeof(listhp[0]);
 	int i, status;
 	
 	printf("Masukan kode hp: ");
 	scanf("%s", &keyword);
 	
 	status = 0;
-	for(i=0; i<size; i++){
+	for(i=0; i<sizeof(f_menu); i++){
 		if(strcasecmp(keyword, listhp[i].kode) == 0){
 			printf("\nNama hp: %s", listhp[i].hp);
 			printf("\nKode hp: %s", listhp[i].kode);
@@ -172,7 +174,8 @@ void MAIN_MENU(){
 	printf("1. List hp\n");
 	printf("2. Cari hp\n");
 	printf("3. Urutkan hp\n");
-	printf("4. Keluar\n");
+	printf("4. Tambah Hp\n");
+	printf("5. Keluar\n");
 }
 
 void SORT_MENU(){
@@ -197,6 +200,31 @@ void SORT_MENU(){
 	}while(anw_user!='3');
 }
 
+void ADD_DATA() {
+	int i = 0;
+	char ch_repeat;
+	do{
+		CLEAR_SCREEN();
+//		SHOW_DATA();
+		
+		f_menu = fopen("data.txt","a");
+		printf("Pengisian Data HP");
+		printf("\nInput HP Name :"); fflush(stdin);gets(listhp[i].hp);
+		printf("\nInput Kode hp :"); fflush(stdin);gets(listhp[i].kode);
+		printf("\nInput stock:"); fflush(stdin);gets(listhp[i].stock);
+		printf("\nInput merek:"); fflush(stdin);gets(listhp[i].merek);
+		printf("\nInput price:"); fflush(stdin);gets(listhp[i].price);
+		
+		fprintf(f_menu, "%s %s %s %s %s\n",listhp[i].hp, listhp[i].kode, listhp[i].stock, listhp[i].price, listhp[i].merek);
+		do {
+			fflush(stdin);
+			printf("Apakah anda ingin menambah data item lagi ? [Y/N] : ");
+			ch_repeat = getche();
+		}while ((ch_repeat!='y')&&(ch_repeat!='Y')&&(ch_repeat!='n')&&(ch_repeat!='N'));
+		fclose(f_menu);
+	}while ((ch_repeat=='y')||(ch_repeat=='Y'));
+}
+
 int main(){
 	char anw_user;
 	do{
@@ -206,16 +234,17 @@ int main(){
 		
 		do{
 			fflush(stdin);
-			printf("\nMasukan pilihan mu [1..4]");
+			printf("\nMasukan pilihan mu [1..5]");
 			anw_user = getche();
 			
-		}while((anw_user!='1')&&(anw_user!='2')&&(anw_user!='3')&&(anw_user!='4'));
+		}while((anw_user!='1')&&(anw_user!='2')&&(anw_user!='3')&&(anw_user!='4')&&(anw_user!='5'));
 		switch(anw_user){
 			case '1': CLEAR_SCREEN(); READ_BY_DEFAULT(); break;
 			case '2': CLEAR_SCREEN(); SEARCH_listhp(); break;
 			case '3': CLEAR_SCREEN(); SORT_MENU(); break;
+			case '4': CLEAR_SCREEN(); ADD_DATA(); break;
 		};
-		if(anw_user!='4')
+		if(anw_user!='5')
 			getche();
-	}while(anw_user!='4');
+	}while(anw_user!='5');
 }
